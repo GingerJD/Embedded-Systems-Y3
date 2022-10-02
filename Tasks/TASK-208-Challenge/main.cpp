dfb8bb778a91e0c1a751ea8e96f8bd39bcaafa1d
Jamie Davis <james.davis-11@students.plymouth.ac.uk>
2022-10-02 19:22:59 +0100

    [02/10/2022] - 19:22 - 207 and 208
    > TASK 207 - COMPLETE
    > TASK 208 - COMPLETE
    
    Unable to get serial monitor to work so will need to email Nic in order to complete stretch task

--- Tasks/TASK-208-Challenge/main.cpp
+++ Tasks/TASK-208-Challenge/main.cpp
@@ -12,32 +12,137 @@ DigitalOut ledGrn(TRAF_GRN1_PIN);
 
 // Timers (modified version from Timer)
 TimerCompat tmr_flash;
-
+TimerCompat sw2debounce;
+TimerCompat sw3debounce;
 // THE CODE BELOW IS NOT A SOLUTION
 //
 // IT IS FUNDAMENTALLY FLAWED (AND INCOMPLETE)
 //
 //
+// Switch states
+typedef enum {
+  WAIT_FOR_PRESS,
+  WAITING_1,
+  WAIT_FOR_REL,
+  WAITING_2
+} SWITCH_STATE;
+
+//set initial states
+SWITCH_STATE sw_state2 = WAIT_FOR_PRESS;
+SWITCH_STATE sw_state3 = WAIT_FOR_PRESS;
 
 int main()
 {
+    
     //Start flashing timer
     tmr_flash.start();
+    
 
     while (true) {
+        long long flash_time = tmr_flash.read_ms();
+        long long sw2_time = sw2debounce.read_ms();
+        long long sw3_time = sw3debounce.read_ms();
 
+        if (flash_time >= 500) {
+            ledYel = !ledYel;
+            tmr_flash.reset();
+        }
+        
+        //Switch 3
+        switch (sw_state3){
+            case WAIT_FOR_PRESS:
+            if (SW3.read() == 1){
+                sw_state3 = WAITING_1;
+                ledRed = !ledRed;
+                sw3debounce.start();
+            }
+            break;
+
+            case WAITING_1:
+            if(sw3_time >= 300){
+                sw_state3 = WAIT_FOR_REL;
+                sw3debounce.stop();
+                sw3debounce.reset();
+            }
+            break;
+
+            case WAIT_FOR_REL:
+            if(SW3.read() == 0){
+                sw_state3 = WAITING_2;
+                sw3debounce.start();
+            }
+            break;
+
+            case WAITING_2:
+            if(sw3_time >=300){
+                sw_state3 = WAIT_FOR_PRESS;
+                sw3debounce.stop();
+                sw3debounce.reset();
+            }
+            break;
+
+            default:
+            sw_state3 = WAIT_FOR_PRESS;
+        }
+        
+        //Switch 2
+        switch (sw_state2){
+            case WAIT_FOR_PRESS:
+            if (SW2.read() == 1){
+                sw_state2 = WAITING_1;
+                ledGrn = !ledGrn;
+                sw2debounce.start();
+            }
+            break;
+
+            case WAITING_1:
+            if(sw2_time >= 300){
+                sw_state2 = WAIT_FOR_REL;
+                sw2debounce.stop();
+                sw2debounce.reset();
+            }
+            break;
+
+            case WAIT_FOR_REL:
+            if(SW2.read() == 0){
+                sw_state2 = WAITING_2;
+                sw2debounce.start();
+            }
+            break;
+
+            case WAITING_2:
+            if(sw2_time >=300){
+                sw_state2 = WAIT_FOR_PRESS;
+                sw2debounce.stop();
+                sw2debounce.reset();
+            }
+            break;
+
+            default:
+            sw_state2 = WAIT_FOR_PRESS;
+        }
+
+        
+        
+        
+        
+        
         //Wait for switch press and release (by BLOCKING)
-        while (SW2.read() == 0);
-        ledRed = !ledRed;
-        wait_us(300000);
+        //while (SW2.read() == 0);
+        //ledRed = !ledRed;
+        //wait_us(300000);
 
-        while (SW2.read() == 1);
-        wait_us(300000);        
+        //while (SW2.read() == 1);
+        //wait_us(300000);        
 
         //Toggle Yellow LED
-        ledYel = !ledYel;
-        while (tmr_flash.read_ms() < 500);
-        tmr_flash.reset();
+        //if(flash_time >=500){
+       //     ledYel = !ledYel;
+       //     tmr_flash.reset();
+       // }
+        //ledYel = !ledYel;
+        //while (tmr_flash.read_ms() < 500);
+        //tmr_flash.reset();
     }
 }
 
--- Tasks/Task-207-Timer/main.cpp
+++ Tasks/Task-207-Timer/main.cpp
@@ -15,6 +15,7 @@ TimerCompat tmr1;
 
 int main()
 {
+    ledRed = 0;
     //Time how long it takes to perform a printf
     tmr1.start();
     printf("Hello World!\n");
@@ -25,23 +26,29 @@ int main()
     printf("The time taken was %llu milliseconds\n", dur);    
 
     //Now to use a timer to implement a delay
-    tmr1.start();
-    while (true) {
+    //tmr1.start();
+    while(true){
+        
         //Wait for switch press
-        while (SW_BLUE == 0);
+        while (SW_BLUE == 0){}
+        
+        //wait_us(5000000);
 
-        //Turn on LED
-        ledRed = 1;
-
+        while(SW_BLUE == 1){
+        tmr1.start();
+        }
+        tmr1.stop();
+        if (tmr1.elapsed_time() > 3000ms){
+            ledRed = !ledRed;
+        }
+        //ledRed = !ledRed;
         //Wait for 500ms
         tmr1.reset();
-        while (tmr1.elapsed_time() < 500ms); //How about this for C++ magic :)
+        // while (tmr1.elapsed_time() < 500ms); //How about this for C++ magic :)
 
-        wait_us(500000);
-
-        //Turn off LED
-        ledRed = 0;
-    }
+       // wait_us(500000);
+    } 
+    
 }
 
 
