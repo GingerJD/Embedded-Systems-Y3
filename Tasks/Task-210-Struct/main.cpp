e200e15866194e382c8f1f8e5f203ddb3a111606
Jamie Davis <james.davis-11@students.plymouth.ac.uk>
2022-10-03 15:37:36 +0100

    [03/10/2022] - 15:36 - 210
    > TASK 210 - COMPLETE
    > TASK 212 - INCOMPLETE
    
    Aim is to complete TASK 212 later today or tomorrow

--- Tasks/Task-210-Struct/main.cpp
+++ Tasks/Task-210-Struct/main.cpp
@@ -1,16 +1,18 @@
 #include "mbed.h"
-
+#include "uop_msb.h"
+using namespace uop_msb;
 // Date Type ComplexNumber_C
 typedef struct {
   double real;
   double imag;
 } ComplexNumber_C;
-
+LCD_16X2_DISPLAY display;
 
 int main() {
 
     //Create instance of a complex number
     ComplexNumber_C p;
+    ComplexNumber_C y;
 
     //Initialise each attribute 
     p.real = 2.0;
@@ -20,10 +22,12 @@ int main() {
     ComplexNumber_C q = {1.0, 1.0};
 
     // TASK:
-    // Create another complex number y
-    // Calculate the complex sum of p and q and store in y
+    y.real = p.real + q.real;
+    y.imag = p.imag + q.imag;
+    display.printf("Y = %.1f+%.1fi",y.real,y.imag);
     // Use printf to display as a complex number (hint: you need two placeholders)
     
     while (true) {
+        //display.printf("Y = %f+%fi",y.real,y.imag);
     }
 }
